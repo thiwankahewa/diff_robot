1ac0ef513822a4f59ff7c2395bd92e7523422647
thiwankahewa <thiwankahewavitharana@gmail.com>
2025-04-12 09:29:42 +0530

    added IMU sensor

--- diff_robot_description/launch/gazebo.launch.py
+++ diff_robot_description/launch/gazebo.launch.py
@@ -64,11 +64,16 @@ def generate_launch_description():
                    "-name", "diff_robot"],
     )
 
+    # Node to bridge topics between ROS 2 and Gazebo
     gz_ros2_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
-            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock", 
+            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU"         
+        ],
+        remappings=[
+            ('/imu', '/imu/out'),  # Remap IMU topic to '/imu/out'
         ]
     )
 
