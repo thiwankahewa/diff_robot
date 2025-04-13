fe02202ef926c2be2e7c1adead4fb4615f673c0d
thiwankahewa <thiwankahewavitharana@gmail.com>
2025-04-13 12:27:17 +0530

    added intermediate twist mux controller

--- diff_robot_controller/launch/joystick_teleop.launch.py
+++ diff_robot_controller/launch/joystick_teleop.launch.py
@@ -3,12 +3,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    
+    diff_robot_controller_pkg_dir = get_package_share_directory('diff_robot_controller')
 
     use_sim_time_arg = DeclareLaunchArgument(name="use_sim_time", default_value="True",
                                       description="Use simulated time"
@@ -30,11 +32,35 @@ def generate_launch_description():
         parameters=[os.path.join(get_package_share_directory("diff_robot_controller"), "config", "joy_config.yaml"),
                     {"use_sim_time": LaunchConfiguration("use_sim_time")}]
     )
+    
+    twist_mux_launch = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("twist_mux"),
+            "launch",
+            "twist_mux_launch.py"
+        ),
+        launch_arguments={
+            "cmd_vel_out": "diff_robot_controller/cmd_vel_unstamped",
+            "config_locks": os.path.join(diff_robot_controller_pkg_dir, "config", "twist_mux_locks.yaml"),
+            "config_topics": os.path.join(diff_robot_controller_pkg_dir, "config", "twist_mux_topics.yaml"),
+            "config_joy": os.path.join(diff_robot_controller_pkg_dir, "config", "twist_mux_joy.yaml"),
+            "use_sim_time": LaunchConfiguration("use_sim_time"),
+        }.items(),
+    )
+
+    twist_relay_node = Node(
+        package="diff_robot_controller",
+        executable="twist_relay.py",
+        name="twist_relay",
+        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}]
+    )
 
     return LaunchDescription(
         [
             use_sim_time_arg,
             joy_teleop,
-            joy_node
+            joy_node,
+            twist_mux_launch,
+            twist_relay_node,
         ]
     )
