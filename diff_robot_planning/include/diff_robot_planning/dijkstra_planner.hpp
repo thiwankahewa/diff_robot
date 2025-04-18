5f9715e68a90263219de2c47c58bf87c06dfe361
thiwankahewa <thiwankahewavitharana@gmail.com>
2025-04-18 13:44:33 +0530

    created cpp path planning files 2

--- diff_robot_planning/include/diff_robot_planning/dijkstra_planner.hpp
+++ diff_robot_planning/include/diff_robot_planning/dijkstra_planner.hpp
@@ -1,16 +1,20 @@
 #ifndef DIJKSTRA_PLANNER_HPP
 #define DIJKSTRA_PLANNER_HPP
 
+#include <string>
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "geometry_msgs/msg/pose.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "geometry_msgs/msg/point.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
-#include "tf2_ros/buffer.h"
-#include "tf2_ros/transform_listener.h"
 
+#include "nav2_core/global_planner.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_util/robot_utils.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav2_costmap_2d/costmap_2d_ros.hpp"
+#include "nav2_msgs/action/smooth_path.hpp"
 
 namespace bumperbot_planning
 {
@@ -39,37 +43,46 @@ struct GraphNode
     }
 };
 
-class DijkstraPlanner : public rclcpp::Node
+class DijkstraPlanner : public nav2_core::GlobalPlanner
 {
 public:
-    DijkstraPlanner();
+  DijkstraPlanner() = default;
+  ~DijkstraPlanner() = default;
+
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+    std::string name, std::shared_ptr<tf2_ros::Buffer> tf,
+    std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+
+  void cleanup() override;
+
+  void activate() override;
+
+  void deactivate() override;
+
+  nav_msgs::msg::Path createPlan(
+    const geometry_msgs::msg::PoseStamped & start,
+    const geometry_msgs::msg::PoseStamped & goal,
+    std::function<bool()> cancel_checker) override;
 
 private:
-    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
-    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  nav2_util::LifecycleNode::SharedPtr node_;
+  nav2_costmap_2d::Costmap2D * costmap_;
+  std::string global_frame_, name_;
+  double interpolation_resolution_;
 
-    nav_msgs::msg::OccupancyGrid::SharedPtr map_;
-    nav_msgs::msg::OccupancyGrid visited_map_;
+  rclcpp_action::Client<nav2_msgs::action::SmoothPath>::SharedPtr smooth_client_;
 
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-
-    void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map);
-
-    void goalCallback(const geometry_msgs::msg::PoseStamped::SharedPtr pose);
-
-    nav_msgs::msg::Path plan(const geometry_msgs::msg::Pose & start, const geometry_msgs::msg::Pose & goal);
-
-    bool poseOnMap(const GraphNode & node);
-
-    GraphNode worldToGrid(const geometry_msgs::msg::Pose & pose);
-
-    geometry_msgs::msg::Pose gridToWorld(const GraphNode & node);
-
-    unsigned int poseToCell(const GraphNode & node);
+  bool poseOnMap(const GraphNode & node);
+  
+  GraphNode worldToGrid(const geometry_msgs::msg::Pose & pose);
+  
+  geometry_msgs::msg::Pose gridToWorld(const GraphNode & node);
+  
+  unsigned int poseToCell(const GraphNode & node);
 };
+
 }  // namespace bumperbot_planning
 
-#endif // DIJKSTRA_PLANNER_HPP
+#endif  // DIJKSTRA_PLANNER_HPP
